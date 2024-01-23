0311fb8a4ff776760891a233d55d8957a8956f70
Pablo Vieira <povieira@gmail.com>
2024-01-23 14:16:26 -0500

    remove logs with sensitive information

--- Season-2/Level-2/code.go
+++ Season-2/Level-2/code.go
@@ -58,7 +58,6 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		password := reqBody.Password
 
 		if !isValidEmail(email) {
-			log.Printf("Invalid email format: %q", email)
 			http.Error(w, "Invalid email format", http.StatusBadRequest)
 			return
 		}
@@ -70,7 +69,6 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		if password == storedPassword {
-			log.Printf("User %q logged in successfully with a valid password %q", email, password)
 			w.WriteHeader(http.StatusOK)
 		} else {
 			http.Error(w, "Invalid Email or Password", http.StatusUnauthorized)
